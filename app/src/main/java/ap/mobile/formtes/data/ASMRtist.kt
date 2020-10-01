8b294549f2f7d2859f96dd8789a33d0b595749c4
alfiputra <alfiputra.id@gmail.com>
2020-10-01 23:17:22 +0700

    progress menu terapi ASMR

---
+++ app/src/main/java/ap/mobile/formtes/data/ASMRtist.kt
@@ -0,0 +1,7 @@
+package ap.mobile.formtes.data
+
+data class ASMRtist(
+    var name: String,
+    var thumbnail: String,
+    var description: String
+)
---
+++ app/src/main/java/ap/mobile/formtes/data/Video.kt
@@ -0,0 +1,7 @@
+package ap.mobile.formtes.data
+
+data class Video(
+    var id : String,
+    var title : String,
+    var duration : Int
+)
